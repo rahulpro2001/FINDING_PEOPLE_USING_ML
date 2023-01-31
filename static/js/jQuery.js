091f40a75168033613b6972064713fc0325a5ee5
unknown <rahulvermapro2001@gmail.com>
2023-02-01 03:12:19 +0530

    changes in templates and statics

--- static/js/jQuery.js
+++
@@ -1,8 +0,0 @@
-$('.loginBtn').onclick(function () {
-    $('.login').show();
-    $('.signUp').hide();
-    /* border bottom on button click */
-    $('.loginBtn').css({ 'border-bottom': '2px solid #ff4141' });
-    /* remove border after click */
-    $('.signUpBtn').css({ 'border-style': 'none' });
-});
